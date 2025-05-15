27355f336597317ed7c37da7b26b04999561916a
Muhirabazram <muhammadirfanaar@gmail.com>
2025-05-15 07:39:14 +0700

    Update header.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">MovieSuper</a>
+      <a class="navbar-brand" href="index.html">MovieSuper</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span class="navbar-toggler-icon"></span>
       </button>
